bc7fc33fd5e350f05143c64243232c75d8d60d45
Wouter van der Merwe <woutervdm21@gmail.com>
2026-03-28 10:04:41 +0200

    Installed EF Core, Created / Migrate DB. Setup EF Core connections DB Context. Bootstrap Icons + Bootswatch setup. Basic styling.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        } 
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
